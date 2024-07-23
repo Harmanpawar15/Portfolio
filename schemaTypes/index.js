31ed164dcca86a3cf7aa0a7c4d559c638f0ef267
Harmanpreet Kaur <80103221+Harmanpawar15@users.noreply.github.com>
2024-07-23 14:56:31 -0400

    Backend With Sanity

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import testimonials from "./testimonials"
+import about from "./about"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
+import workExperience from "./workExperience"
+import works from "./works"
+export const schemaTypes = [testimonials,about,contact,experiences,skills , workExperience, works]
