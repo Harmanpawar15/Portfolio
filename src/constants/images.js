2ca8be04308f308df6370c26ecd918380321e937
Harmanpreet Kaur <80103221+Harmanpawar15@users.noreply.github.com>
2024-08-04 00:57:35 -0400

    Projects Section Added

--- src/constants/images.js
+++ src/constants/images.js
@@ -17,6 +17,7 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import geeklogo from '../assets/geeklogo.png';
 
 import about01 from '../assets/about01.png';
 import about02 from '../assets/about02.png';
@@ -25,6 +26,7 @@ import about04 from '../assets/about04.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
+import propic from '../assets/propic.png' ;
 
 import logo from '../assets/logo.png';
 
@@ -70,4 +72,6 @@ export default {
   nb,
   skype,
   spotify,
+  propic,
+ geeklogo,
 };
