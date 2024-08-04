ab69842fe554508322a1450ba85e96426e985263
Harmanpreet Kaur <80103221+Harmanpawar15@users.noreply.github.com>
2024-08-04 05:11:46 -0400

    Final Edits

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { urlFor, client } from '../../client';
 import {AppWrap, MotionWrap} from '../../wrapper'
 
-const About = () => {
+const About= () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
